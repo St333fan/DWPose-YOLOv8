873dcbdd87353023d775c679b41e1f6e3c27252c
St333fan <96583923+St333fan@users.noreply.github.com>
2024-04-10 14:17:11 +0200

    Update __init__.py

--- ControlNet-v1-1-nightly/annotator/dwpose/__init__.py
+++ ControlNet-v1-1-nightly/annotator/dwpose/__init__.py
@@ -66,3 +66,46 @@ class DWposeDetector:
             pose = dict(bodies=bodies, hands=hands, faces=faces)
 
             return draw_pose(pose, H, W)
+
+class DWposeDetector2D:
+    def __init__(self):
+
+        self.pose_estimation = Wholebody()
+
+    def __call__(self, oriImg):
+        oriImg = oriImg.copy()
+
+        H, W, C = oriImg.shape
+        with torch.no_grad():
+            candidate, subset = self.pose_estimation(oriImg)
+            nums, keys, locs = candidate.shape
+            candidate[..., 0] /= float(W)
+            candidate[..., 1] /= float(H)
+            body = candidate[:, :18].copy()
+            body = body.reshape(nums * 18, locs)
+            score = subset[:, :18]
+            for i in range(len(score)):
+                for j in range(len(score[i])):
+                    if score[i][j] > 0:
+                        score[i][j] = int(18 * i + j)
+                    else:
+                        score[i][j] = -1
+
+            un_visible = subset < 0
+
+            arr_2d = np.full((nums, keys), True)
+            arr_2d[:1, 0:18] = un_visible[:1, 0:18]
+            un_visible = arr_2d
+            candidate[un_visible] = -1
+
+            foot = candidate[:, 18:24]
+
+            faces = candidate[:, 24:92]
+
+            hands = candidate[:, 92:113]
+            hands = np.vstack([hands, candidate[:, 113:]])
+
+            bodies = dict(candidate=body, subset=score)
+            pose = dict(bodies=bodies, hands=hands, faces=faces)
+
+            return draw_pose(pose, H, W), pose, body
